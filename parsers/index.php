6486c2a91b4996ab155d38c9e9c307c6e3e59ced
Igor <ifranzhev@gmail.com>
2015-10-28 21:22:13 +0100

    Change permissions

--- parsers/index.php
+++ parsers/index.php
@@ -1,9 +1,8 @@
 <?php
 include_once dirname(__FILE__).'/../protected-parsers/extensions/functions.php';
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../yii/framework/yii.php';
+$yii=dirname(__FILE__).'/../../yii/framework/yii.php';
 $config=dirname(__FILE__).'/../protected-parsers/config/main.php';
-
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
